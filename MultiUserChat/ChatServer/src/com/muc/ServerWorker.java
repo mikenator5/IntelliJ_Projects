9cf041b140b30ac1ae8ba50b409263f971b016a1
Mikenator5 <Mikenator5@gmail.com>
2019-05-03 12:49:14 -0600

    Refactored code to handle more ServerWorkers at once. Added new class "Server" to do so

--- MultiUserChat/ChatServer/src/com/muc/ServerWorker.java
+++ MultiUserChat/ChatServer/src/com/muc/ServerWorker.java
@@ -1,14 +1,24 @@
 package com.muc;
 
-import java.io.*;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Date;
+import java.util.List;
 
 public class ServerWorker extends Thread {
 
     private final Socket clientSocket;
+    private final Server server;
+    private String login = null;
+    private String password = null;
+    private OutputStream outputStream;
 
-    public ServerWorker(Socket clientSocket) {
+    public ServerWorker(Server server, Socket clientSocket) {
+        this.server = server;
         this.clientSocket = clientSocket;
 
     }
@@ -26,18 +36,67 @@ public class ServerWorker extends Thread {
 
     private void handleClientSocket() throws IOException, InterruptedException {
         InputStream inputStream = clientSocket.getInputStream();
-        OutputStream outputStream  = clientSocket.getOutputStream();
+        this.outputStream  = clientSocket.getOutputStream();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
         while ((line = reader.readLine()) != null) {
-            if ("quit".equalsIgnoreCase(line)) {
-                break;
+
+            // Also Works: String[] tokens = line.split(" ");
+
+            // Had to import from Apache Commons class. Found download on internet, extracted the .jar, and added the module from Project Structure.
+            String[] tokens = StringUtils.split(line);
+
+            if (tokens != null && tokens.length > 0) {
+                String cmd = tokens[0];
+                if ("quit".equalsIgnoreCase(line)) {
+                    break;
+                } else if ("login".equalsIgnoreCase(cmd)) {
+                    handleLogin(outputStream, tokens);
+                } else {
+                    String msg = "unknown " + cmd + "\n";
+                    outputStream.write(msg.getBytes());
+                }
+                /*String msg = "You typed: " + line + "\n";
+                outputStream.write(msg.getBytes());*/
             }
-            String msg = "You typed: " + line + "\n";
-            outputStream.write(msg.getBytes());
         }
 
         clientSocket.close();
     }
-}
+
+    public String getLogin() {
+        return login;
+    }
+
+    private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+
+        if (tokens.length == 3) {
+            String login = tokens[1];
+            String password = tokens[2];
+
+            if ( (login.equals("guest") && password.equals("guest")) || (login.equals("Mike") && password.equals("Yeet")) ) {
+                String msg = "ok login\n";
+                outputStream.write(msg.getBytes());
+                this.login = login;
+                System.out.println("User logged in successfully: " + login);
+
+                String onlineMsg = "online " + login + "\n";
+                List<ServerWorker> workerList = server.getWorkerList();
+                for (ServerWorker worker : workerList) {
+                    worker.send(onlineMsg);
+                }
+
+            } else {
+                String msg = "error login\n";
+                outputStream.write(msg.getBytes());
+            }
+        }
+
+    } // End of handleLogin
+
+    private void send(String msg) throws IOException {
+        outputStream.write(msg.getBytes());
+    }
+
+} // End of Class ServerWorker
