a2686cafca37a37921d90ca0c2f772b31caf1680
Mikenator5 <Mikenator5@gmail.com>
2019-05-21 12:38:00 -0600

    Created new class "UserListPane", to create the GUI for the chat client. Only set up the basics so far, and added a constructor to add all users currently online and display them on the GUI.

---
+++ MultiUserChat/ChatClient/src/com/muc/UserListPane.java
@@ -0,0 +1,57 @@
+package com.muc;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class UserListPane extends JPanel implements UserStatusListener {
+
+    private final ChatClient client;
+    private JList<String> userListUI;
+    private DefaultListModel<String> userListModel;
+
+
+    public UserListPane(ChatClient client) {
+        this.client = client;
+        this.client.addUserStatusListener(this); // Second "this" added this class to the ListArray
+
+        userListModel = new DefaultListModel<>();
+        userListUI = new JList<>(userListModel);
+        setLayout(new BorderLayout());
+        add(new JScrollPane(userListUI), BorderLayout.CENTER);
+    }
+
+    // Set up the basic Java Swing GUI
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient("localhost", 8818);
+
+        UserListPane userListPane = new UserListPane(client);
+        JFrame frame = new JFrame("User List");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 600);
+
+        frame.getContentPane().add(userListPane, BorderLayout.CENTER);
+        frame.setVisible(true);
+
+        if (client.connect()) {
+            try {
+                client.login("guest", "guest");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    // Required classes due to the interface
+    @Override
+    public void online(String login) {
+        userListModel.addElement(login);
+    }
+
+    @Override
+    public void offline(String login) {
+        userListModel.removeElement(login);
+    }
+}
